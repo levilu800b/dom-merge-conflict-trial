821370e56707f3ba13529e1c81028c79996dfccf
levilu800b <levilu800b@gmail.com>
2024-02-24 13:32:33 +0000

    preview-short-form

--- tasks/conditional-rendering/src/profilePreview.js
+++ tasks/conditional-rendering/src/profilePreview.js
@@ -1,22 +1,25 @@
 export function ProfilePreview(previewInfo) {
-  const preview = document.createElement("aside");
+  const preview = document.createElement('aside');
 
-  const picture = document.createElement("img");
+  const picture = document.createElement('img');
   picture.src = previewInfo.profileInfo.pictureSrc;
-  picture.alt = "";
-  picture.dataset.testid = "profilePicture";
+  picture.alt = '';
+  picture.dataset.testid = 'profilePicture';
 
-  const name = document.createElement("p");
+  const name = document.createElement('p');
   name.textContent = previewInfo.profileInfo.name;
-  name.dataset.testid = "profileName";
+  name.dataset.testid = 'profileName';
 
-  const bio = document.createElement("p");
-  bio.textContent = previewInfo.profileInfo.bio;
-  bio.dataset.testid = "profileBio";
+  if (!previewInfo.shortForm) {
+    // Render bio only if not in short form
+    const bio = document.createElement('p');
+    bio.textContent = previewInfo.profileInfo.bio;
+    bio.dataset.testid = 'profileBio';
+    preview.appendChild(bio);
+  }
 
   preview.appendChild(picture);
   preview.appendChild(name);
-  preview.appendChild(bio);
 
   return preview;
 }
