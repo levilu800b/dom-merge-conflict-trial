b0ccbf1baf71617206b5031052138e667077dec1
levilu800b <levilu800b@gmail.com>
2024-02-24 13:02:46 +0000

    add-decrement-button

--- tasks/buttons-and-counter/src/app.js
+++ tasks/buttons-and-counter/src/app.js
@@ -1,44 +1,15 @@
-//increments the number in a node's text
-function increment(node) {
-  let current = node.textContent;
-  node.textContent = Number(current) + 1;
-}
-
-//decrements the number in a node's text
-function decrement(node) {
-  let current = node.textContent;
-  node.textContent = Number(current) - 1;
-}
+// App.js
+import { createHeader } from './header';
+import { createMainContent } from './mainContent';
 
 export function App() {
   const body = document.createElement('body');
 
-  const header = document.createElement('header');
-  header.innerHTML = `
-        <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one. Press decrement to decrease the count by one.</p>
-    `;
+  const header = createHeader();
   body.appendChild(header);
 
-  const buttonAndCounter = document.createElement('main');
-  buttonAndCounter.innerHTML = `
-        <p id="counter" data-testid="counter">0</p>
-        <button id="increment">Increment</button>
-        <button id="decrement">Decrement</button>
-    `;
-  body.appendChild(buttonAndCounter);
-
-  const incrementButton = body.querySelector('#increment');
-  const decrementButton = body.querySelector('#decrement');
-  const counter = body.querySelector('#counter');
-
-  incrementButton.addEventListener('click', () => {
-    increment(counter);
-  });
-
-  decrementButton.addEventListener('click', () => {
-    decrement(counter);
-  });
+  const mainContent = createMainContent();
+  body.appendChild(mainContent);
 
   return body;
 }
--- tasks/buttons-and-counter/src/header.js
+++ tasks/buttons-and-counter/src/header.js
@@ -3,7 +3,7 @@ export function createHeader() {
   const header = document.createElement('header');
   header.innerHTML = `
         <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
+        <p>A simple counter. Press increment to increase the count by one. Press decrement to decrease the count by one.</p>
     `;
   return header;
 }
